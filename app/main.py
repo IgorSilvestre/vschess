1dbf50b6b91a16518d450eb9167e4fdb99761cda
Igor Silvestre <igor@igorsilvestre.dev>
2025-10-26 19:42:17 -0300

    chore: add railway frontend to cors allowance

--- app/main.py
+++ app/main.py
@@ -33,6 +33,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1",
         "http://127.0.0.1:3000",
+        "https://vschess-frontend-production.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
